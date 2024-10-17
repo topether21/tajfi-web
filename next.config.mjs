30affe431c056c748faad54611d90974b28995b6
topether21 <topether21@gmail.com>
2024-10-17 17:00:08 -0600

    chore: remove yarn

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	jslint: {
+		// Warning: This allows production builds to successfully complete even if
+		// your project has ESLint errors.
+		ignoreDuringBuilds: true,
+	},
 	images: {
+		domains: ["placehold.co", "raw.githubusercontent.com"],
 		remotePatterns: [
 			{
 				protocol: "https",
--- src/components/containers/marketplace/list-asset-item.tsx
+++ src/components/containers/marketplace/list-asset-item.tsx
@@ -5,14 +5,14 @@ import { CELL_PADDING, LIST_ITEM_HEIGHT, LIST_ITEM_WIDTH } from './contants';
 export const ListAssetItem = ({ index, style, assets }: { index: number; style: React.CSSProperties; assets: Asset[] }) => (
     <div style={{ ...style, padding: CELL_PADDING }} className="p-4 border-b">
         <div className="flex items-center">
-            {/* <Image
+            <Image
                 src={assets[index].image}
                 alt={assets[index].name}
                 width={LIST_ITEM_WIDTH}
                 height={LIST_ITEM_HEIGHT}
                 style={{ width: 'auto', height: 'auto' }}
                 className="object-cover mr-4"
-            /> */}
+            />
             <div>
                 <h3 className="text-lg font-semibold">{assets[index].name}</h3>
                 <p className="text-sm text-gray-600">{assets[index].price} BTC</p>
