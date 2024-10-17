d18d91b1ad2b3d8115103353a9d8dbc3743383d1
topether21 <topether21@gmail.com>
2024-10-17 17:16:07 -0600

    chore: without filter

--- src/components/containers/marketplace/use-assets.ts
+++ src/components/containers/marketplace/use-assets.ts
@@ -21,7 +21,7 @@ const fetchAssets = async (start: number, size: number) => {
             id,
             name: `Asset ${id}`,
             price: Math.floor(Math.random() * 100) / 100,
-            image: `https://placehold.co/600x400?text=${id}`,
+            image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id + 1}.png`,
             satoshiPrice: 100000000,
             ordinalNumber: 1,
             types: ["normal"],
