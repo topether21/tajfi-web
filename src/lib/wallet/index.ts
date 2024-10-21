76135d8da2920f1160e57da85e52dfb94b3dda3b
topether21 <topether21@gmail.com>
2024-10-20 23:56:02 -0600

    debug signing

--- src/lib/wallet/index.ts
+++ src/lib/wallet/index.ts
@@ -1,7 +1,8 @@
 'use client';
 import * as bitcoin from 'bitcoinjs-lib';
 import ecc from '@bitcoinerlab/secp256k1';
-import { PSBT_HEX } from './data';
+import * as tools from 'uint8array-tools';
+import { PSBT_HEX, SIGNED_PSBT } from './data';
 
 bitcoin.initEccLib(ecc);
 
@@ -47,8 +48,59 @@ const getAddressInfo = (pubkey: string) => {
     return addrInfo;
 }
 
+// https://github.com/bitcoinjs/bitcoinjs-lib/blob/master/src/cjs/psbt/bip371.cjs#L105
+function serializeTaprootSignature(sig: Uint8Array, sighashType?: number) {
+    const sighashTypeByte = sighashType
+        ? Uint8Array.from([sighashType])
+        : Uint8Array.from([]);
+    return tools.concat([sig, sighashTypeByte]);
+}
 
-const signInvoice = async (ordinalsPublicKey: string) => {
+const signInvoiceV2 = async (ordinalsPublicKey: string) => {
+    const hexPsbt = PSBT_HEX;
+    const psbt = bitcoin.Psbt.fromHex(hexPsbt);
+    console.log("original base64", psbt.toBase64());
+    const psbtSigned = bitcoin.Psbt.fromHex(SIGNED_PSBT);
+    console.log("signed base64", psbtSigned.toBase64());
+    const addressInfo = getAddressInfo(ordinalsPublicKey);
+    console.log("addressInfo", addressInfo);
+
+    for (const input of psbt.data.inputs) {
+        console.log("input", input);
+        // debugger;
+    };
+
+    // Uncaught (in promise) Error: Invalid arguments for Psbt.updateInput. Cannot use both taproot and non-taproot fields.
+    // @jad
+    psbt.updateInput(0, {
+        tapInternalKey: Buffer.from(ordinalsPublicKey, 'hex'), // Use the internal key for Taproot
+        witnessUtxo: {
+            script: addressInfo.output,
+            value: 56n,
+        },
+    });
+
+    const sigHash = psbt.__CACHE.__TX.hashForWitnessV1(
+        0,
+        [addressInfo.output],
+        [56n],
+        bitcoin.Transaction.SIGHASH_DEFAULT
+    );
+    console.log("sigHash", sigHash);
+    const sigHashHex = Buffer.from(sigHash).toString('hex');
+    console.log("sigHashHex", sigHashHex);
+    const signed = await window.nostr?.signSchnorr(sigHashHex);
+    console.log("signed", signed);
+    psbt.updateInput(0, {
+        tapKeySig: Buffer.from(signed, 'hex'),
+    });
+    psbt.finalizeAllInputs();
+    const updatedPsbtHex = psbt.toHex();
+    console.log("Updated PSBT Hex:", updatedPsbtHex);
+};
+
+
+const signInvoiceV1 = async (ordinalsPublicKey: string) => {
     const hexPsbt = PSBT_HEX;
     const psbt = bitcoin.Psbt.fromHex(hexPsbt);
     console.log("base64", psbt.toBase64());
@@ -73,7 +125,7 @@ const signInvoice = async (ordinalsPublicKey: string) => {
     console.log({ signatureUint8Array, pubkeyUint8Array })
 
     psbt.updateInput(0, {
-        finalScriptSig: signatureUint8Array,
+        finalScriptSig: Buffer.from(signatureUint8Array),
     });
 
     // Finalize the input
@@ -90,7 +142,7 @@ export const connectWallet = async (provider = '') => {
     const ordinalsPublicKey = await getNostrPubKey();
     // const serverAuthResponse = await auth(ordinalsPublicKey);
     // console.log("serverAuthResponse", serverAuthResponse);
-    await signInvoice(ordinalsPublicKey);
+    await signInvoiceV2(ordinalsPublicKey);
     return {
         walletName,
         ordinalsPublicKey,
