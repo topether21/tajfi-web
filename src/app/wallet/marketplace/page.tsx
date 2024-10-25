4469a9c92be6161811c326401052a52cfabca56e
topether21 <topether21@gmail.com>
2024-10-24 21:18:11 -0600

    chore: enable demo marketplace

--- src/app/wallet/marketplace/page.tsx
+++ src/app/wallet/marketplace/page.tsx
@@ -1,9 +1,19 @@
 import { WalletAssets } from '@/components/containers/wallet/wallet-assets'
+import { Skeleton } from '@/components/ui/skeleton'
+import React, { Suspense } from 'react'
 
 export default function Marketplace() {
   return (
     <div className="h-screen flex flex-col">
-      <WalletAssets />
+      <Suspense
+        fallback={
+          <div className="flex-grow flex items-center justify-center">
+            <Skeleton className="h-12 w-full rounded-md" />
+          </div>
+        }
+      >
+        <WalletAssets />
+      </Suspense>
     </div>
   )
 }
