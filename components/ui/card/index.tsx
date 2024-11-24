a4a891247a25a5cdc0067a05069f252f0117db88
topether21 <topether21@gmail.com>
2024-11-24 11:41:18 -0600

    chore: update gluestack-ui

--- components/ui/card/index.tsx
+++ components/ui/card/index.tsx
@@ -1,23 +1,23 @@
-import React from "react";
-import type { VariantProps } from "@gluestack-ui/nativewind-utils";
-import { View, ViewProps } from "react-native";
-import { cardStyle } from "./styles";
+import React from 'react';
+import type { VariantProps } from '@gluestack-ui/nativewind-utils';
+import { View, ViewProps } from 'react-native';
+import { cardStyle } from './styles';
 
 type ICardProps = ViewProps &
-	VariantProps<typeof cardStyle> & { className?: string };
+  VariantProps<typeof cardStyle> & { className?: string };
 
 const Card = React.forwardRef<React.ElementRef<typeof View>, ICardProps>(
-	({ className, size = "md", variant = "elevated", ...props }, ref) => {
-		return (
-			<View
-				className={cardStyle({ size, variant, class: className })}
-				{...props}
-				ref={ref}
-			/>
-		);
-	},
+  ({ className, size = 'md', variant = 'elevated', ...props }, ref) => {
+    return (
+      <View
+        className={cardStyle({ size, variant, class: className })}
+        {...props}
+        ref={ref}
+      />
+    );
+  }
 );
 
-Card.displayName = "Card";
+Card.displayName = 'Card';
 
 export { Card };
--- components/ui/center/index.tsx
+++ components/ui/center/index.tsx
@@ -1,22 +1,22 @@
-import { View, ViewProps } from "react-native";
-import React from "react";
-import { centerStyle } from "./styles";
-import type { VariantProps } from "@gluestack-ui/nativewind-utils";
+import { View, ViewProps } from 'react-native';
+import React from 'react';
+import { centerStyle } from './styles';
+import type { VariantProps } from '@gluestack-ui/nativewind-utils';
 
 type ICenterProps = ViewProps & VariantProps<typeof centerStyle>;
 
 const Center = React.forwardRef<React.ElementRef<typeof View>, ICenterProps>(
-	({ className, ...props }, ref) => {
-		return (
-			<View
-				className={centerStyle({ class: className })}
-				{...props}
-				ref={ref}
-			/>
-		);
-	},
+  ({ className, ...props }, ref) => {
+    return (
+      <View
+        className={centerStyle({ class: className })}
+        {...props}
+        ref={ref}
+      />
+    );
+  }
 );
 
-Center.displayName = "Center";
+Center.displayName = 'Center';
 
 export { Center };
