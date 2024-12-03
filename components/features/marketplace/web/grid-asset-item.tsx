cba4196f0b9be12383baf6b13195b437099d5a6f
topether21 <topether21@gmail.com>
2024-12-03 16:21:13 -0600

    chore: init buy

--- components/features/marketplace/web/grid-asset-item.tsx
+++ components/features/marketplace/web/grid-asset-item.tsx
@@ -6,8 +6,12 @@ import { MIN_CARD_HEIGHT } from './constants'
 import { Image } from 'expo-image'
 import { Box } from '@/components/ui/box'
 import { Text } from '@/components/ui/text'
-import { Pressable, StyleSheet } from 'react-native'
+import { StyleSheet } from 'react-native'
+import { Pressable } from "@/components/ui/pressable"
 import { router } from 'expo-router'
+import { useStore } from '@nanostores/react'
+import { $checkoutAssetIds, addCheckoutAssetId, removeCheckoutAssetId } from '@/store/checkout-store'
+import { Plus } from 'lucide-react-native'
 
 interface GridAssetItemProps {
   item: Asset
@@ -17,23 +21,36 @@ export const GridAssetItem = ({ item }: GridAssetItemProps) => {
   const formatSatoshis = (satoshis: number) => {
     return (satoshis / 100000000).toFixed(8)
   }
-
+  const checkoutAssetIds = useStore($checkoutAssetIds);
+  const isChecked = checkoutAssetIds.find(id => id === item.id);
+  const goToAsset = () => router.push(`/marketplace/${item.id}`);
+  const toggleCheckout = () => {
+    console.log('toggleCheckout', item.id, isChecked, checkoutAssetIds);
+    if (isChecked) {
+      console.log('removing from checkout', item.id);
+      removeCheckoutAssetId(item.id);
+    } else {
+      console.log('adding to checkout', item.id);
+      addCheckoutAssetId(item.id);
+    }
+  }
+  console.log('checkoutAssetIds', checkoutAssetIds);
   return (
-    <Pressable onPress={() => router.push(`/marketplace/${item.id}`)}>
+    <Pressable>
       <Card
-        className="m-2 overflow-hidden transition-all duration-300 hover:shadow-lg relative rounded-md"
+        className="m-2 overflow-hidden relative rounded-t-lg border"
         style={{ height: MIN_CARD_HEIGHT }}
       >
-        <Box className="absolute top-2 right-2">
-          <Badge className="bg-background-tajfi-deep-blue text-white p-1 rounded-full">
-            <Check size={16} />
+        <Pressable className="absolute top-2 right-2 z-10" onPress={toggleCheckout}>
+          <Badge className="bg-background-tajfi-deep-blue text-white p-1 rounded-full" >
+            {isChecked ? <Check size={16} /> : <Plus size={16} />}
           </Badge>
-        </Box>
+        </Pressable>
         <Image
           source={item.image}
           alt={item.name}
           contentFit="contain"
-          style={styles.image}
+          className="select-none h-[85%] w-full"
           transition={500}
         />
         <Box>
